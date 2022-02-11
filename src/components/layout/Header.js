40248fae51c88d474b05783d9e7c467500a924ad
asztandar <sztandaradam@gmail.com>
2022-02-11 19:55:55 +0100

    STEG-13 add header classess

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,11 @@
 import * as React from "react";
 import Logo from "./Logo";
 import Menu from "./Menu";
+import * as classess from "@styles/module/header.module.css";
 
 const Header = () => {
     return (
-        <header>
+        <header className={classess.header}>
             <Logo />
             <Menu />
         </header>
