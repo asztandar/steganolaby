5ffe6f07c92414abe9ffeea747c92df40c422e28
asztandar <sztandaradam@gmail.com>
2022-02-11 21:22:01 +0100

    STEG-12 modify structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { Helmet } from "react-helmet";
+import Layout from "../components/layout/Layout";
 
 const IndexPage = () => {
     return (
-        <main>
+        <Layout>
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>SteganoLaby - Strona główna</title>
             </Helmet>
-        </main>
+        </Layout>
     );
 };
 
