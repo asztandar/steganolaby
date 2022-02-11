8f52c51b7eef5c974fde381d8f3ad96cd658f94d
asztandar <sztandaradam@gmail.com>
2022-02-11 19:16:45 +0100

    STEG-5 add links

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -1,9 +1,43 @@
 import * as React from 'react';
+import { Link } from "gatsby"
+import * as classess from '@styles/module/menu.module.css';
 
-const Menu = () =>{
+const links = [
+    {
+        text:"Strona główna",
+        url: "/",
+        title: "Strona główna projektu",
+    },
+    {
+        text:"Zakoduj",
+        url: "/encode.js",
+        title: "Zakoduj wiadomość w grafice",
+    },
+    {
+        text:"Odkoduj",
+        url: "/decode.js",
+        title: "Odkoduj wiadomość z grafiki",
+    },
+    {
+        text:"Sprawdzanie",
+        url: "/check.js",
+        title: "Opcja jedynie dla prowadzącego",
+    }
+]
+
+const Menu = () => {
     return(
-        ''
+        <nav className={classess.menu}>
+            <ul>
+                {links.map(link => (
+                    <li key={link.url}>
+                        <Link to={link.url} title={link.title}>{link.text}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     );
+
 }
 
 export default Menu;
