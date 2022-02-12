4c489eae5788c192a62faa0bc87794657942ec1d
asztandar <sztandaradam@gmail.com>
2022-02-12 12:34:58 +0100

    STEG-14 added footer to layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,16 @@
-import * as React from 'react';
+import * as React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import "@styles/main.css";
 
-const Layout = () => {
-    return('');
-}
+const Layout = (props) => {
+    return (
+        <main>
+            <Header />
+            <section>{props.children}</section>
+            <Footer />
+        </main>
+    );
+};
 
 export default Layout;
