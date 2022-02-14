37d03a71aac11b8c1af866e4f6206812160abc94
asztandar <sztandaradam@gmail.com>
2022-02-14 13:04:52 +0100

    STEG-12 add responsive

--- src/components/layout/Menu.js
+++ src/components/layout/Menu.js
@@ -1,43 +1,90 @@
-import * as React from 'react';
-import { Link } from "gatsby"
-import * as classess from '@styles/module/menu.module.css';
+import * as React from "react";
+import { Link } from "gatsby";
+import * as classess from "@styles/module/menu.module.css";
 
 const links = [
     {
-        text:"Strona główna",
+        text: "Strona główna",
         url: "/",
         title: "Strona główna projektu",
     },
     {
-        text:"Zakoduj",
+        text: "Zakoduj",
         url: "/encode.js",
         title: "Zakoduj wiadomość w grafice",
     },
     {
-        text:"Odkoduj",
+        text: "Odkoduj",
         url: "/decode.js",
         title: "Odkoduj wiadomość z grafiki",
     },
     {
-        text:"Sprawdzanie",
+        text: "Sprawdzanie",
         url: "/check.js",
         title: "Opcja jedynie dla prowadzącego",
-    }
-]
+    },
+];
+
+function toggleMobileMenu() {
+    const menu = document.querySelector(`.${classess.hamburger_icon}`);
+    console.log("menu: ", menu)
+    menu.classList.toggle(`${classess.open}`);
+
+
+    const menuList = document.querySelector(`.${classess.menuMobile}`);
+    menuList.classList.toggle(`${classess.open}`);
+}
 
 const Menu = () => {
-    return(
-        <nav className={classess.menu}>
-            <ul>
-                {links.map(link => (
-                    <li key={link.url}>
-                        <Link to={link.url} title={link.title} activeClassName={classess.active_link}>{link.text}</Link>
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    );
+        return (
+            <nav className={classess.menu}>
+                <div className={classess.menuScreen}>
+                    <ul>
+                        {links.map((link) => (
+                            <li key={link.url}>
+                                <Link to={link.url} title={link.title} activeClassName={classess.active_link}>
+                                    {link.text}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div
+                    className={classess.hamburger_icon}
+                    onClick={toggleMobileMenu}
+                    onKeyDown={toggleMobileMenu}
+                    role="presentation"
+                >
+                    <div className={`${classess.bar1} ${classess.bar}`}></div>
+                    <div className={`${classess.bar2} ${classess.bar}`}></div>
+                    <div className={`${classess.bar3} ${classess.bar}`}></div>
+                    <div className={classess.menuMobile}>
+                        <ul>
+                            {links.map((link) => (
+                                <li key={link.url}>
+                                    <Link to={link.url} title={link.title} activeClassName={classess.active_link} >
+                                        {link.text}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        );
+    };
 
-}
+    // return(
+    //     <nav className={classess.menu}>
+    //         <ul>
+    //             {links.map(link => (
+    //                 <li key={link.url}>
+    //                     <Link to={link.url} title={link.title} activeClassName={classess.active_link}>{link.text}</Link>
+    //                 </li>
+    //             ))}
+    //         </ul>
+    //     </nav>
+    // );
+
 
 export default Menu;
