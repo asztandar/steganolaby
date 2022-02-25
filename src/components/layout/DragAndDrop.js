afcacfe395c38f3d8cfb53d8ec26085139d767b3
asztandar <sztandaradam@gmail.com>
2022-02-25 18:29:48 +0100

    STEG-17 added drag and drop component

--- src/components/layout/DragAndDrop.js
+++ src/components/layout/DragAndDrop.js
@@ -1,20 +1,74 @@
 import * as React from "react";
 import * as classess from "@styles/module/drag_and_drop.module.css";
 import cloud from "../../images/cloud_1.svg"
+import wrong from "../../images/warning.svg"
+import Modal from "./Modal";
+
+
+
 const DragAndDrop = () =>{
+
+    const [showModal, setShowModal] = React.useState(false);
+
+    const acceptedExtension = ['bmp'];
+    let file = null;
+    let fileType = "";
+
+    const handleDragOver = (e) =>{
+        e.preventDefault();
+        console.log("over");
+        document.querySelector(`.${classess.dnd}`).classList.add(`${classess.active}`);
+    }
+    
+    const handleDragLeave = (e) =>{
+        e.preventDefault();
+        console.log("leave");
+        document.querySelector(`.${classess.dnd}`).classList.remove(`${classess.active}`);
+    }
+    
+    const handleDrop = (e) =>{
+        e.preventDefault();
+        e.stopPropagation();
+        console.log("drop");
+        file = e.dataTransfer.files[0];
+        fileType = file.type.split('/').pop().toLowerCase();
+        if(acceptedExtension.includes(fileType)){
+            let fileReader = new FileReader();
+            fileReader.onload = () =>{
+                let fileURL = fileReader.result;
+                console.log(fileURL);
+                let imgTag = `<img src="${fileURL}" alt="your picture" class="${classess.yourImage}">`;
+                // document.getElementById('dropArea').innerHTML = "";
+                document.getElementById('dropArea').innerHTML = imgTag;
+            }
+            fileReader.readAsDataURL(file)
+
+        }
+        else{
+            badFormat();
+        }
+    }
+
+    function badFormat(){
+        setShowModal(prev => !prev);
+    }
+
     return (
-        <div className={classess.dnd}>
-            <figure>
-                <img src={cloud} alt="cloud" className={classess.cloud}/>
-            </figure>
-            <span className={classess.text}>
-                Przeciągnij i upuść tutaj
-                <br />
-                lub
-                <br />
-                <button>Przeglądaj</button>
-            </span>
-        </div>
+        <article className={classess.container}>
+            <div id="dropArea" className={classess.dnd} onDragOver={handleDragOver} onDragLeave={handleDragLeave} onDrop={handleDrop} role="presentation">
+                <figure>
+                    <img src={cloud} alt="cloud" className={classess.cloud}/>
+                </figure>
+                <span className={classess.text}>
+                    Przeciągnij i upuść tutaj
+                    <br />
+                    lub
+                    <br />
+                    <button id="butt">Przeglądaj</button>
+                </span>
+                <Modal showModal={showModal} setShowModal={setShowModal} alt="wrong" text="Sorry, wrong format" image={wrong}/>
+            </div>
+        </article>
     );
 }
 
--- src/components/layout/Modal.js
+++ src/components/layout/Modal.js
@@ -1,50 +1,56 @@
 import * as React from "react";
 import * as classess from "@styles/module/modal.module.css";
-import { MdClose } from 'react-icons/md';
+import { MdClose } from "react-icons/md";
 
 const Modal = ({ showModal, setShowModal, image, alt, text }) => {
     const modalRef = React.useRef();
 
-    const closeModal = (e) =>{
-        if(modalRef.current === e.target){
+    const closeModal = (e) => {
+        if (modalRef.current === e.target) {
             setShowModal(false);
         }
     };
 
     const keyPress = React.useCallback(
-        e => {
-          if (e.key === 'Escape' && showModal) {
-            setShowModal(false);
-            console.log('I pressed');
-          }
+        (e) => {
+            if (e.key === "Escape" && showModal) {
+                setShowModal(false);
+                console.log("I pressed");
+            }
         },
         [setShowModal, showModal]
-      );
+    );
 
-    
-      React.useEffect(
-        () => {
-          document.addEventListener('keydown', keyPress);
-          return () => document.removeEventListener('keydown', keyPress);
-        },
-        [keyPress]
-      );
+    React.useEffect(() => {
+        document.addEventListener("keydown", keyPress);
+        return () => document.removeEventListener("keydown", keyPress);
+    }, [keyPress]);
 
     return (
         <>
-        {showModal ? (
-            <article className={classess.background} onClick={closeModal} ref={modalRef}>
-                <div className={classess.modal}>
-                    <img src={image} alt={alt} />
-                    <div className={classess.content}>
-                        <p>{text}</p>
+            {showModal ? (
+                <article
+                    className={classess.background}
+                    onClick={closeModal}
+                    ref={modalRef}
+                    onKeyDown={closeModal}
+                >
+                    <div className={classess.modal}>
+                        <img src={image} alt={alt} />
+                        <div className={classess.content}>
+                            <p>{text}</p>
+                        </div>
                     </div>
-                </div>
-                <div className={classess.closeButton} onClick={() => setShowModal(prev=> !prev)}>
-                    <MdClose/>
-                </div>
-            </article>
-        ) : null}
+                    <div
+                        className={classess.closeButton}
+                        onClick={() => setShowModal((prev) => !prev)}
+                        role="presentation"
+                        onKeyDown={() => setShowModal((prev) => !prev)}
+                    >
+                        <MdClose />
+                    </div>
+                </article>
+            ) : null}
         </>
     );
 };
